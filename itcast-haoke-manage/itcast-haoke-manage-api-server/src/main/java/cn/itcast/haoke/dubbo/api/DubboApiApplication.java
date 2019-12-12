07a613595ffcca4082137e4bb354b8180b607b12
wuda5 <2252316187@qq.com>
2019-12-12 15:29:06 +0800

    想测试--打了标签的--下次拉取标签的代码会不会有--这次修改的内容： 正确的应该是不会有的

--- itcast-haoke-manage/itcast-haoke-manage-api-server/src/main/java/cn/itcast/haoke/dubbo/api/DubboApiApplication.java
+++ itcast-haoke-manage/itcast-haoke-manage-api-server/src/main/java/cn/itcast/haoke/dubbo/api/DubboApiApplication.java
@@ -8,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DubboApiApplication {
 
     public static void main(String[] args) {
+
+        
         SpringApplication.run(DubboApiApplication.class, args);
+        System.out.println("++++ test tag [api server start ok!]++++");
     }
 }
