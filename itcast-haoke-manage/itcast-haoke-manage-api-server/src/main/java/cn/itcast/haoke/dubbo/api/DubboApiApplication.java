698e4c4636bfe5dfa87673bfd6b127efd900e380
wuda5 <2252316187@qq.com>
2019-12-12 15:02:56 +0800

    test

---
+++ itcast-haoke-manage/itcast-haoke-manage-api-server/src/main/java/cn/itcast/haoke/dubbo/api/DubboApiApplication.java
@@ -0,0 +1,13 @@
+package cn.itcast.haoke.dubbo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// --注意：对外暴露的端口是：18080
+@SpringBootApplication
+public class DubboApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboApiApplication.class, args);
+    }
+}
