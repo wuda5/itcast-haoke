b2a8a75f997476d253e9fa1cea4b8e04cdfff1e0
wuda5 <2252316187@qq.com>
2019-12-12 15:13:02 +0800

    test2

---
+++ itcast-haoke-manage/itcast-haoke-manage-api-server/src/main/java/cn/itcast/haoke/dubbo/api/controller/HouseResourcesController.java
@@ -0,0 +1,52 @@
+package cn.itcast.haoke.dubbo.api.controller;
+
+import cn.itcast.haoke.dubbo.api.service.HouseResourcesService;
+import cn.itcast.haoke.dubbo.server.pojo.HouseResources;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("house/resources")
+public class HouseResourcesController {
+
+    @Autowired
+    private HouseResourcesService houseResourcesService;
+
+    /**
+     * 新增房源
+     *
+     * @param houseResources json数据
+     * @return
+     */
+    @PostMapping
+    @ResponseBody
+    public ResponseEntity<Void> save(@RequestBody HouseResources houseResources) {
+            System.out.println("+++++ 新增房源 插入 start ");
+        try {
+            boolean bool = this.houseResourcesService.save(houseResources);
+            if (bool) {
+            System.out.println("+++++ 新增房源--- insert 成功：【"+ResponseEntity.status(HttpStatus.CREATED).build());
+                return ResponseEntity.status(HttpStatus.CREATED).build();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
+    /**
+     * test
+     *
+     * @return
+     */
+    @GetMapping
+    @ResponseBody
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("ok");
+    }
+}
